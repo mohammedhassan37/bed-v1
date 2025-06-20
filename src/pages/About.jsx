bcaf8e58538f1a0ce949e5a080fc77868763cda3
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-06-20 03:18:15 +0100

    Contact page updates and added a todo file
    I added content to the contact page mainly, i was able to add the contact form, not fully setup yet, which is logged in the TODO text file which i also added. which will help me keep track of projects. this principle of logging what i need to do inside the folder/outside where it is secured allows me to be able to ensure i klnow what to do next.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,9 +3,7 @@ import '../styles/About.css'
 function About() {
   return <div>
     <h2>About Page</h2>
-    <p>This is the about page content.</p>
-    <p>yoooo</p>
-  </div>;
+  </div>
 }
 
 export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,11 +5,40 @@ function Contact() {
   return (
     <>
     <div className="contact-container">
-      <div className="contact-items">
-        <h1>Contact us</h1>
-        <label htmlFor="email">Enter your email</label>
-        <input type="text" id='email'/>
-      </div>
+    <div className="contact-left">
+          <h2>Have a question?</h2>
+          <p>
+          we're here to help. Fil in the form for technical support. Via phone 
+          or Email. Our team is here to help you. We will provide you support,
+          whatever your inquiry is.
+          </p>
+          <p>
+          To ensure that our clinets needs are met, please allow us 24hrs.
+          From the start of the next business day to ensure we can properly
+          specialise our help to you. Our working business hours are 9-5. M-F.
+          </p>
+          <p>helpline@gmail.com</p>
+          <p>07213 132111</p>
+    </div>
+    <div className="contact-right">
+        <form action="">
+              <input type="text"  placeholder='*First Name' id='name' required/>
+          <input type="text" placeholder='*Last Name' id='name' required/>
+    <div className="contact-right-itmes">
+          <input type="Email" placeholder='*Email' required/>
+          <input type="text" placeholder='Phone Number (optional)'/>
+          <select name="category" id="" required>
+            <option value="">*Select your topic</option>
+            <option value="">Beds</option>
+            <option value="">Billings</option>
+            <option value="">Refunds</option>
+            <option value="">General Inquiry</option>
+          </select>
+          <textarea name="" id="" placeholder='*Message' required></textarea>
+    </div>
+      <button>SUBMIT</button>
+        </form>
+    </div>
     </div>
     </>
   );
